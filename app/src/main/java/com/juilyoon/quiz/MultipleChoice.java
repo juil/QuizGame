dcefb96de57de4491f637691a023ddd3d64f58d1
Juil Yoon <jiy.juil@gmail.com>
2016-06-28 19:03:37 -0400

    Add randomizeOptions function to MultipleChoice class

--- app/src/main/java/com/juilyoon/quiz/MultipleChoice.java
+++ app/src/main/java/com/juilyoon/quiz/MultipleChoice.java
@@ -2,6 +2,7 @@ package com.juilyoon.quiz;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by juil on 16-06-27.
@@ -17,6 +18,23 @@ public class MultipleChoice extends Question{
 
     public String[] getOptions() {
         return options;
+    }
+
+    /**
+     * randomizeOptions shuffles the order of the question's multiple choice options
+     *
+     * Fisher-Yates algorithm taken from http://stackoverflow.com/a/18456998/745776
+     */
+    public void randomizeOptions() {
+        int index;
+        String temp;
+        Random random = new Random();
+        for (int i = answers.length - 1; i > 0; i--) {
+            index = random.nextInt(i + 1);
+            temp = answers[index];
+            answers[index] = answers[i];
+            answers[i] = temp;
+        }
     }
 
     /**
