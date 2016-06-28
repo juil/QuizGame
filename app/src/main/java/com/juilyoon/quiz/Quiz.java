b667e17c61197c50d3b926a6a4b42e3df1c95749
Juil Yoon <jiy.juil@gmail.com>
2016-06-28 18:41:07 -0400

    Add quiz class

---
+++ app/src/main/java/com/juilyoon/quiz/Quiz.java
@@ -0,0 +1,20 @@
+package com.juilyoon.quiz;
+
+import java.util.Queue;
+
+/**
+ * Created by juil on 16-06-27.
+ */
+public class Quiz {
+    private Question[] questions;
+
+    public Quiz(Question[] questions) {
+        this.questions = questions;
+    }
+
+    public void randomizeOptions(Question question) {
+
+    }
+}
+
+
