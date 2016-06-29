3fcb0dfca15fa72135f291d67ba80e6091dceaee
Juil Yoon <jiy.juil@gmail.com>
2016-06-29 00:27:32 -0400

    Add single and multi multiple choice, and text answer views.

--- app/src/main/java/com/juilyoon/animequiz/QuizActivity.java
+++ app/src/main/java/com/juilyoon/animequiz/QuizActivity.java
@@ -3,26 +3,48 @@ package com.juilyoon.animequiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioGroup;
+import com.juilyoon.quiz.*;
+
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
     // Quiz metadata
     protected int quizLength = 10;
-    protected int[] correctAnswers = new int[quizLength];
     // Question data
     private int questionNum = 1;
+    private Quiz quizFMA;
     // Question views
-//    RadioGroup multipleChoice_single = (RadioGroup) findViewById(R.id.multiple_choice_single);
+    RadioGroup singleMultipleChoice;
+    ListView multiMultipleChoice;
+    EditText textAnswer;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // Question views
+        singleMultipleChoice = (RadioGroup) findViewById(R.id.single_multipleChoice);
+        multiMultipleChoice = (ListView) findViewById(R.id.multi_multipleChoice);
+        textAnswer = (EditText) findViewById(R.id.text_answer);
+
+        // Example quiz
+        Question q1 = new Question("What is the most important law of alchemy?", new String[]{"The Law of Equivalent Exchange", "Equivalent Exchange"});
+        quizFMA = new Quiz(new Question[]{q1});
     }
 
-    private void nextQuestion() {
-//        multipleChoice_single.setVisibility(View.GONE);
+    public void nextQuestion(View view) {
+        clearQuestion();
+    }
+
+    private void clearQuestion() {
+        singleMultipleChoice.setVisibility(View.GONE);
+        multiMultipleChoice.setVisibility(View.GONE);
+        textAnswer.setVisibility(View.GONE);
     }
 }
