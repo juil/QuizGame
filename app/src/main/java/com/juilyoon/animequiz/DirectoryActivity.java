eb97ed29de73055c4c389379122688943a539bec
Juil Yoon <jiy.juil@gmail.com>
2016-06-29 21:51:00 -0400

    todo: Set persistent best score

--- app/src/main/java/com/juilyoon/animequiz/DirectoryActivity.java
+++ app/src/main/java/com/juilyoon/animequiz/DirectoryActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DirectoryActivity extends AppCompatActivity {
 
@@ -12,6 +15,9 @@ public class DirectoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directory);
+
+        TextView bestScore = (TextView) findViewById(R.id.quiz_fma_bestScore);
+        bestScore.setText("Best: "); //#TODO: Store and retrieve best score on quiz.
     }
 
     public void openQuiz(View view) {
